d4d4be6875567c4a7e1416e5b7f1d6dfd57b0e0e
Ruben-Torres02 <ruben.torres@correounivalle.edu.co>
2024-11-11 16:13:04 -0500

    Actualizacion de la pagina de inicio

--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -1,38 +1,42 @@
-import { Canvas } from "@react-three/fiber"
-import Button from "../../components/Button/Button"
-import ButtonLogOut from "../../components/ButtonLogOut/ButtonLogOut"
-import "./Start.css"
-import { Trash } from "../../components/Trash/Trash"
-import { Link } from "react-router-dom"
-const Start = () =>{
-    return (
-        <>
-        <ButtonLogOut />
-        <div className="container">
-          <div className="container-options">
-            <div className="container-options-option">
-              <Link to={"/pollution"}>
-                <Button text={"Contaminacion"}/>
-              </Link>
-            </div>
+// Start.js
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import "./Start.css";
+import { Model } from "../../components/ocean/Ocean";
+import WelcomeText from "./welcomeText/WelcomeText";
 
-            <div className="container-options-option">
-              <Link to={"/shortage"}>
-              <Button text={"Escasez"}/>
-              </Link>
-            </div>
+const Start = () => {
+  // Configuración de la cámara
+  const cameraSettings = {
+    position: [0, 185, 0],  // Posición de la cámara (ajustada para estar por debajo del modelo)
+    fov: 80,                 // Campo de visión
+  };
 
-            <div className="container-options-option">
-              <Button text={"Acidificacion"}/>
-            </div>
-            <div className="container-options-option">
-              <Button text={"Test"}/>
-            </div>
-          </div>
-        </div>
-      </> 
-    )
+  return (
+    <div className="canvas-container">
+      <Canvas
+        camera={cameraSettings}
+        style={{ height: "100vh", width: "100vw" }}
+      >
+        {/* Configuración de iluminación */}
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} />
 
-}
+        {/* Modelo */}
+        <Model />
+        <WelcomeText />
 
-export default Start
+        {/* Controles de cámara con límites */}
+        <OrbitControls
+          target={[0, 180, -20]} // Asegura que la cámara mire hacia el centro de la escena
+          maxDistance={120}   // Límite máximo de distancia de la cámara
+          minDistance={120}    // Límite mínimo de distancia de la cámara
+          maxPolarAngle={Math.PI / 1.5}  // Limita el ángulo vertical para evitar que la cámara vea hacia abajo
+          minPolarAngle={Math.PI / 4}  // Limita el ángulo vertical para evitar que la cámara vea hacia abajo
+        />
+      </Canvas>
+    </div>
+  );
+};
+
+export default Start;
